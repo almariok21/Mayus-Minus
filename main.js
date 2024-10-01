63a1d20876a05a2e665dda595342c69b6e8e02ab
almariok21 <almariok21@gmail.com>
2024-10-01 11:05:23 -0500

    okay

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 const btnMayusculas = document.querySelector('.btn-mayusculas');
 const btnMinusculas = document.querySelector('.btn-minusculas');
 const btnCapitalizar = document.querySelector('.btn-capitalizar');
+const btnCopiar = document.querySelector('.btn-copiartext');
 const outputText = document.querySelector('#outputText');
 
 btnMayusculas.addEventListener('click', convertToMayusculas);
 btnMinusculas.addEventListener('click', convertToMinusculas);
 btnCapitalizar.addEventListener('click', capitalizeText);
+btnCopiar.addEventListener('click', copyText);
 
-function updateOutput() {
-    outputText.innerHTML = document.getElementById('inputText').value;
+function copyText() {
+    const textArea = document.getElementById('inputText');
+    navigator.clipboard.writeText(textArea.value);
+    btnCopiar.innerHTML = 'Copiado!';
+    setTimeout(() => {
+        btnCopiar.innerHTML = 'Copiar el Texto';
+    }, 500);
 }
 
 
 function convertToMayusculas() {
     const textArea = document.getElementById('inputText');
     textArea.value = textArea.value.toUpperCase();
-    updateOutput();
 }
 
 function convertToMinusculas() {
     const textArea = document.getElementById('inputText');
     textArea.value = textArea.value.toLowerCase();
-    updateOutput();
 }
 
 function capitalizeText() {
@@ -29,7 +34,4 @@ function capitalizeText() {
     textArea.value = textArea.value.replace(/\w\S*/g, function(txt){
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
-    updateOutput();
 }
-
-updateOutput();
